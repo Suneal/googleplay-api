725608024d899b8c128a0c2cd4d4aa78da19ec7c
Sunil  Manandhar <sunil@th006-4.cs.wm.edu>
2018-03-19 18:26:37 -0400

    gets apps based on title separately

--- title_run_server.py
+++ title_run_server.py
@@ -37,7 +37,7 @@ counter = 0
 keywords = ['smart home', 'smarthome', 'home smart', 'smart home system', 'smart home device', 'smart home automate', 'home automation', 'automate home', 'home automation system', 'home automation device', 'smart system', 'internet of things', 'iot', 'web of things']
 
 for keyword in keywords:
-    apps = server.search(keyword, 3004, None)   
+    apps = server.search(keyword, 3004, None)
     print("Fetching apps for ", keyword , "...")
     with open(keyword + '_jsondump.json', 'a') as json_file:
         json_file.write('[')
@@ -48,26 +48,34 @@ for keyword in keywords:
                 if (counter %20 ==0):
                     print(time.strftime('%M:%S'), '...')
 
-                if(package_name not in package_name_list):                
+                if(package_name not in package_name_list):
                     title = a['title']
                     author = a['author']
                     author_without_comma = str.replace(str(author),',','-')
 
                     details = server.details(package_name)
+
+                    description = str.replace(str(details['description']),',','-' )
+                    description = description.encode('ascii',
+                    'ignore').decode('ascii')
                     title_list.append(title)
 
-                    
-                    json_file.write(json.dumps(details))
+
+
+                    json_file.write('[' +  json.dumps(a) + ',' +json.dumps(details) + ']')
                     json_file.write(',')
-                    
+
                     if(package_name not in package_name_list):
                         package_name_list.append(package_name)
-                    
+
                     if (author not in author_list):
                         author_list.append(author)
-                    
-                    the_file.write(package_name+ ',' + title  + ',' + author_without_comma)
+                    the_file.write(package_name.encode('ascii',
+'ignore').decode('ascii')+ ',' + title.encode('ascii',
+'ignore').decode('ascii')  + ',' + author_without_comma.encode('ascii',
+'ignore').decode('ascii') +',' + description)
                     the_file.write('\n')
+                    time.sleep(1)
         json_file.write(']')
         print('number of results: %d for %s -- Author List: %d' % (len(apps), keyword, len(author_list)))
 
@@ -80,7 +88,7 @@ with open('UniqueList.csv', 'a') as uniqueDump:
 
 
 
-print("\n Authors: \n")
+print("\n Title \n")
 #Get unique authors --> this gives more than we ask
 #Get unique packagenames
 #See if keyword in details
@@ -94,8 +102,8 @@ for title in title_list:
     batch = BATCH_SIZE_TITLE
     if(title_counter %BATCH_SIZE_TITLE ==0):
         batch += BATCH_SIZE_TITLE
-    with open('title/'+keyword +'_'+ str(batch) + '_' + '_applist_secondary.csv', 'a') as the_file: 
-        with open('title/'+keyword +'_'+ str(batch) + '_' + '_secondary_jsondump.json', 'a') as json_file:
+    with open('title/'+ 'new' +'_'+ str(batch) + '_' + '_applist_secondary.csv', 'a') as title_file:
+        with open('title/'+'new' +'_'+ str(batch) + '_' + '_secondary_jsondump.json', 'a') as json_file:
             print('number of results: %d for %s' % (len(new_apps), title))
             for a in new_apps:
                 package_name = a['docId']
@@ -106,6 +114,9 @@ for title in title_list:
                 author_without_comma = str.replace(str(author),',','-')
                 details = server.details(package_name)
                 details_description = details['description']
+                description = str.replace(str(details['description']),',','-' )
+                description = description.encode('ascii',
+                'ignore').decode('ascii')
                 #sleep(0.2)
                 if(package_name not in package_name_list):
                     package_name_list.append(package_name)
@@ -114,19 +125,16 @@ for title in title_list:
                             print(package_name)
                             app_counter +=1
 
-                            authors_file.write( package_name+ ',' + title  + ',' + author_without_comma)
-                            authors_file.write('\n')
-                 
-                            title_json_file.write(json.dumps(details))
-                            title_json_file.write(',')
+                            title_file.write( package_name.encode('ascii',
+'ignore').decode('ascii')+ ',' + title.encode('ascii',
+'ignore').decode('ascii')  + ',' + author_without_comma.encode('ascii',
+'ignore').decode('ascii')+',' + description)
+                            title_file.write('\n')
+
+                            json_file.write('[' + json.dumps(a)  + ',' + json.dumps(details) + ']')
+
+#                            title_json_file.write(json.dumps(details))
+                            json_file.write(',')
                             break
-                sleep(1) #Because of the rate limit            
+                sleep(1) #Because of the rate limit
             json_file.write(']')
-
-                
-
-
-
-
-
-
